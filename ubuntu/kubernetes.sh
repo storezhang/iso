3c8b0ff892e248b9b8dd36e5c597052c6b2cab68
storezhang <storezhang@gmail.com>
2025-01-11 17:47:50 +0800

    refactor(项目): 重构

--- ubuntu/kubernetes.sh
+++ ubuntu/kubernetes.sh
@@ -26,6 +26,14 @@ check() {
 }
 
 auth() {
+    root_password=$1
+    username=$2
+    password=$3
+
+    if [[ -n ${root_password} ]]; then # 配置根账号密码
+        customize "${basedir}" 设置根用户密码 echo "root:${root_password}" | chpasswd
+    fi
+
     if [[ -n ${username} ]]; then # 创建用户
         gid=1000
         uid=1000
@@ -36,20 +44,15 @@ auth() {
     if [[ -n ${password} ]]; then # 配置密码
         customize "${basedir}" 设置用户密码 echo "${username}:${password}" | chpasswd
     fi
-
-    if [[ -n ${root_password} ]]; then # 配置根账号密码
-        customize "${basedir}" 设置根用户密码 echo "root:${root_password}" | chpasswd
-    fi
 }
 
 prepare() {
     arch=$1
     version=$2
-    packages=$3
-    basedir=$4
+    basedir=$3
+    packages=$4
 
     codename=$(curl -s https://changelogs.ubuntu.com/meta-release | grep Name: | tail -n1)
-
     log INFO 准备文件系统 "filepath=${basedir}"
     sudo debootstrap --arch="${arch}" --include="${packages}" --variant=minbase "${codename}" "${basedir}" https://mirrors.ustc.edu.cn/ubuntu
 
@@ -71,30 +74,29 @@ prepare() {
     trap 'sudo unmount ${dev} ${proc} ${sys}' EXIT
 }
 
+execute() {
+    log INFO 开始定制系统 "root=${basedir}"
+    customize "${basedir}" 更新系统 apt update
+    customize "${basedir}" 升级系统 apt upgrade
+    customize "${basedir}" 使用Bash环境 chsh --shell /usr/bin/bash
+    customize "${basedir}" 设置时间为重庆 apt install tzdata -y && cp /usr/share/zoneinfo/Asia/Chongqing /etc/localtime
+    customize "${basedir}" 安装SSH服务器并 apt install openssh-server -y
+    customize "${basedir}" 开启ROOT账号登录权限 echo "PermitRootLogin yes" > /etc/ssh/sshd_config.d/root
+}
+
 kubernetes() {
     before # 准备执行环境
 
     workspace="${3:-kubernetes}"
     check "${workspace}"
 
-    basedir="${workspace}/ubuntu-${version}-${arch}"
-
-    version=${1:-24.10}
     arch=${2:-amd64}
+    version=${1:-24.10}
+    basedir="${workspace}/ubuntu-${version}-${arch}"
+    prepare "${arch}" "${version}" "${basedir}" "bash,locales"
 
     root_password=${4}
     username=${5:-kubernetes}
     password=${6}
-
-  packages="bash"
-  packages="${packages},locales"
-
-
-    log INFO 开始定制系统 "root=${basedir}"
-  customize "${basedir}" 更新系统 apt update
-  customize "${basedir}" 升级系统 apt upgrade
-  customize "${basedir}" 使用Bash环境 chsh --shell /usr/bin/bash
-  customize "${basedir}" 设置时间为重庆 apt install tzdata -y && cp /usr/share/zoneinfo/Asia/Chongqing /etc/localtime
-  customize "${basedir}" 安装SSH服务器并 apt install openssh-server -y
-  customize "${basedir}" 开启ROOT账号登录权限 echo "PermitRootLogin yes" > /etc/ssh/sshd_config.d/root
+    auth "${workspace}" "${root_password}" "${username}" "${password}"
 }
