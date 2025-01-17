a35d255cd1d9defe7630538309251fcd66a75a66
storezhang <storezhang@gmail.com>
2025-01-17 13:51:08 +0800

    refactor(项目): 变量命名规范

--- core/chroot.sh
+++ core/chroot.sh
@@ -5,10 +5,10 @@ source core/log.sh
 
 chroot() {
     name=$1
-    directory=$2
+    basedir=$2
     commands=${*:2+1}
 
-    log DEBUG 开始执行定制命令 "name=${name}, directory=${directory}, commands=${commands}"
-    sudo /sbin/chroot "${directory}" sh -c "${commands}" || exit 1
-    log INFO 定制命令成功 "name=${name}, directory=${directory}, commands=${commands}"
+    log DEBUG 开始执行定制命令 "name=${name}, basedir=${basedir}, commands=${commands}"
+    sudo /sbin/chroot "${basedir}" sh -c "${commands}" || exit 1
+    log INFO 定制命令成功 "name=${name}, basedir=${basedir}, commands=${commands}"
 }
