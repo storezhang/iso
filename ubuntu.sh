78b78ce57d5a8efc73ba1d6abbfb6bb6a6a0279e
storezhang <storezhang@gmail.com>
2025-01-21 18:40:57 +0800

    refactor(项目): 使用4个空格

--- ubuntu.sh
+++ ubuntu.sh
@@ -13,33 +13,34 @@ longs="version:arch:type:workspace:username:password:root-password-password:rp:m
 args=$(getopt --longoptions ${longs} --options ${shorts} --alternative -- "$@")
 eval set -- "$args"
 while true; do
+    value=${2#*=}
     case "$1" in
         -v|--version)
-            version=${2#*=}
+            version=${value}
             ;;
         -a|--arch)
-            arch=${2#*=}
+            arch=${value}
             ;;
         -w|--workspace)
-            workspace=${2#*=}
+            workspace=${value}
             ;;
         -t|--type)
-            type=${2#*=}
+            type=${value}
             ;;
         -u|--username)
-            username=${2#*=}
+            username=${value}
             ;;
         -p|--password)
-            password=${2#*=}
+            password=${value}
             ;;
         -r|--rp|--root-password-password)
-            root_password=${2#*=}
+            root_password=${value}
             ;;
         -m|--mirror)
-            mirror=${2#*=}
+            mirror=${value}
             ;;
         -c|--cleanup)
-            cleanup=${2#*=}
+            cleanup=${value}
             ;;
         --)
             shift
