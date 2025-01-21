33da7d304283bf1a188ab53d387ca10ba515a0df
storezhang <storezhang@gmail.com>
2025-01-21 10:05:02 +0800

    refactor(项目): 使用4个空格

--- ubuntu.sh
+++ ubuntu.sh
@@ -13,48 +13,48 @@ longs="version:arch:type:workspace:username:password:root-password-password:rp:m
 args=$(getopt --longoptions ${longs} --options ${shorts} --alternative -- "$@")
 eval set -- "$args"
 while true; do
-  case "$1" in
-    -v|--version)
-      version=${2#*=}
-      ;;
-    -a|--arch)
-      arch=${2#*=}
-      ;;
-    -w|--workspace)
-      workspace=${2#*=}
-      ;;
-    -t|--type)
-      type=${2#*=}
-      ;;
-    -u|--username)
-      username=${2#*=}
-      ;;
-    -p|--password)
-      password=${2#*=}
-      ;;
-    -r|--rp|--root-password-password)
-      root_password=${2#*=}
-      ;;
-    -m|--mirror)
-        mirror=${2#*=}
-      ;;
-    -c|--cleanup)
-      cleanup=${2#*=}
-      ;;
-    --)
-      shift
-      break;;
-  esac
-shift
+    case "$1" in
+        -v|--version)
+            version=${2#*=}
+            ;;
+        -a|--arch)
+            arch=${2#*=}
+            ;;
+        -w|--workspace)
+            workspace=${2#*=}
+            ;;
+        -t|--type)
+            type=${2#*=}
+            ;;
+        -u|--username)
+            username=${2#*=}
+            ;;
+        -p|--password)
+            password=${2#*=}
+            ;;
+        -r|--rp|--root-password-password)
+            root_password=${2#*=}
+            ;;
+        -m|--mirror)
+            mirror=${2#*=}
+            ;;
+        -c|--cleanup)
+            cleanup=${2#*=}
+            ;;
+        --)
+            shift
+            break;;
+    esac
+    shift
 done
 
 log INFO "开始镜像定制"
 case "${type}" in
-  kubernetes)
-    log INFO "开始定制Kubernetes镜像"
-    source ubuntu/kubernetes.sh
-    kubernetes "${arch}" "${version}" "${workspace}" "${root_password}" "${username}" "${password}" "${mirror}" "${cleanup}"
-    ;;
-  --)
-    ;;
+    kubernetes)
+        log INFO "开始定制Kubernetes镜像"
+        source ubuntu/kubernetes.sh
+        kubernetes "${arch}" "${version}" "${workspace}" "${root_password}" "${username}" "${password}" "${mirror}" "${cleanup}"
+        ;;
+    --)
+        ;;
 esac
